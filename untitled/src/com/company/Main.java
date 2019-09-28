36199c56ee25335ad573a2510a1dc02d96f25398
ybf-mm <xiaobu12>
2019-09-28 17:21:31 +0800

    66666666666666666666666666666666666666666666

--- untitled/src/com/company/Main.java
+++ untitled/src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Integer[] nums = {-1,0,1,2,-1,-4000};
+        Integer[] nums = {-1,0,1,2,-100,-4000};
         int i=0;
         List<List<Integer>> threeSum = threeSum(nums);
         System.out.println(threeSum);
